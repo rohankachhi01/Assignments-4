7d138536486d271e93fe703d51ade60c74edaf2d
rohan kachhi <rohangaming192@gmail.com>
2025-10-07 19:13:20 +0530

    Update asssignment4.py

--- asssignment4.py
+++ asssignment4.py
@@ -10,4 +10,25 @@ try:
 except FileNotFoundError:
     print(f"Error: The file '{a}' was not found.")
 except Exception as e:
+
     print(f"An error occurred: {e}")
+
+#task 2: Write and Append Data to a File
+
+    filename = "sample.txt"
+
+    try:
+        with open(filename, "w") as f:
+            f.write("This is the first line.\n")
+            f.write("This is the second line.\n")
+        print("Data written to file successfully.")
+    except Exception as e:
+        print(f"An error occurred while writing: {e}")
+
+    # Append data to the file
+    try:
+        with open(filename, "a") as f:
+            f.write("This is an appended line.\n")
+        print("Data appended to file successfully.")
+    except Exception as e:
+        print(f"An error occurred while appending: {e}")
